c6ddec5cd59b1178616bac5a2044f999c9b7df38
Tim Butterfield <124673267+timio23@users.noreply.github.com>
2023-05-15 22:56:05 +0100

    Create layout.vue

---
+++ src/layout.vue
@@ -0,0 +1,44 @@
+<template>
+	<div v-if="!loading">
+		<p>Collection: {{ collection }}</p>
+
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item in items" :key="item.id">
+					<td>{{ item.name }}</td>
+					<td>{{ item.email }}</td>
+					<td><a :href="`/content/${collection}/${item.id}`">View Details</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+export default {
+	inheritAttrs: false,
+	props: {
+		collection: {
+			type: String,
+			required: true,
+		},
+		items: Array,
+		loading: Boolean,
+		error: Array,
+		search: {
+			type: String,
+			default: null,
+		},
+	},
+	setup(props) {
+
+	}
+};
+</script>
